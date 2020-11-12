0241a8d4375f9a9de06ae381e0ae8bf41c0b0fd6
Vish Singh <vishalicious213@gmail.com>
2020-11-11 17:57:57 -0800

    setting up Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+      <div id="header">
+          <div>
+              <img src='' alt='Mayiha Hoque'></img>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Header;
