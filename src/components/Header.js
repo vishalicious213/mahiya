6777e195bb4913927ba8a4b0145f6aab75c21877
Vish Singh <vishalicious213@gmail.com>
2020-11-11 20:08:53 -0800

    Header image stopped working. Added separate import.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo17 from '../img/logo17.png'
 
 function Header() {
     return (
       <div id="header">
           <div id='logo-container'>
-            <img id='logo' src='../img/logo17.png' alt='Mahiya Hoque'></img>
+            <img id='logo' src={logo17} alt='Mahiya Hoque'></img>
           </div>
       </div>
     )
