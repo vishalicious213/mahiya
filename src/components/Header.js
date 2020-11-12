2443419cd2cc816af467262dba7012373b4f6534
Vish Singh <vishalicious213@gmail.com>
2020-11-11 19:27:15 -0800

    drafted Projects

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Header() {
     return (
       <div id="header">
-          <div>
-            <img src='../img/logo17.png' alt='Mahiya Hoque'></img>
+          <div id='logo-container'>
+            <img id='logo' src='../img/logo17.png' alt='Mahiya Hoque'></img>
           </div>
       </div>
     )
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Projects() {
+    return (
+      <div id="header">
+          <div id='byline-container'>
+            <p id='byline'>My mission is to create communities with <span id='highlight'>compassion, empathy, and understanding.</span></p>
+          </div>
+      </div>
+    )
+  }
+  
+  export default Projects;
