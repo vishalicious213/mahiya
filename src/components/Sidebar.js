6291cefe3c2502932f2275af388fafb0b18d5a78
Vish Singh <vishalicious213@gmail.com>
2020-11-13 01:10:28 -0800

    reverted 'about me' link to go to original site so i can build out routes and components for them w/o mahiya losing functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 function Sidebar() {
     return (
       <div id="sidebar">
-        {/* <a href='http://www.mahiya.design/about-me/'>
+        <a href='http://www.mahiya.design/about-me/'>
           <p className='side-menu-item'>about me</p>
-        </a> */}
-        <Link to='/about-me'>
+        </a>
+        {/* <Link to='/about-me'>
           <p className='side-menu-item'>about me</p>
-        </Link>
+        </Link> */}
         <a href='http://www.mahiya.design/my-thoughts/'>
           <p className='side-menu-item'>my thoughts</p>
         </a>
