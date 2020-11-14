5027136c4f47d2a1d40e24d77c43c330198f1863
Vish Singh <vishalicious213@gmail.com>
2020-11-14 01:15:42 -0800

    rebuilt footer to show social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react'
+// import globe from '../img/footer/globe.svg'
+// import linkedin from '../img/footer/linkedin.svg'
+// import medium from '../img/footer/medium.svg'
+// import email from '../img/footer/email.svg'
+import '../styles/footer.css'
 
 function Footer() {
     return (
-      <div id="footer">
-          <p id='copyright'>Copyright © 2020, Mahiya Hoque</p>
+      // <div id="footer">
+      //     <p id='copyright'>Copyright © 2020, Mahiya Hoque</p>
+      // </div>
+      <div id='footer'>
+        <div className='footer-icons' id='globe-icon'></div>
+        <div className='footer-icons' id='linkedin-icon'></div>
+        <div className='footer-icons' id='medium-icon'></div>
+        <div className='footer-icons' id='email-icon'></div>
+        {/* <img className='footer-icons' src={linkedin} alt='LinkedIn icon'></img>
+        <img className='footer-icons' src={medium} alt='Medium icon'></img>
+        <img className='footer-icons' src={email} alt='Email icon'></img> */}
       </div>
     )
 }
-  
-  export default Footer;
+
+export default Footer;
