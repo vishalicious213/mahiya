1f1a35b7ab8354a07be6e1199afc6544c09a1e56
Vish Singh <vishalicious213@gmail.com>
2020-11-13 23:04:45 -0800

    added home arrow/button to bottom of sidebar in desktop view

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,7 @@
-// import './App.css';
 import React from 'react'
+import aboutme from '../img/about-me.jpg'
+import activity1 from '../img/activity-tracker-app1.png'
+import meal1 from '../img/mealventory-app1.png'
 
 function AboutMe() {
   return (
@@ -8,7 +10,12 @@ function AboutMe() {
             <p id='byline'>Hi there, I’m Mahiya and my mission is to create a sense of community.</p>
         </div>
 
-        <p className='text-content'>I have worked within the customer service fields in retail, car sales, and administrative positions, where I constantly needed to understand people. For years, I have practiced creating empathy for the problems we face routinely. This naturally brought me to the field of product design where I now solve these problems using design thinking. </p>
+        <div id='about-me-top'>
+            <p className='text-content'>I have worked within the customer service fields in retail, car sales, and administrative positions, where I constantly needed to understand people. For years, I have practiced creating empathy for the problems we face routinely. This naturally brought me to the field of product design where I now solve these problems using design thinking. </p>
+            <div id='about-me-img-container'>
+                <img id='about-me-img' src={aboutme} alt='Mahiya Hoque'></img>
+            </div>
+        </div>
 
         <h3 className='text-header'>What problems am I currently invested in?</h3>
         <p className='text-content'>The year 2020 has emboldened a lot of societal and political issues that had already existed. The ones I’m occupied with currently are voting-transparency and anti-racism.</p>
@@ -18,6 +25,12 @@ function AboutMe() {
 
         <h3 className='text-header'>I would love to meet you to hear about your adventures!</h3>
         <p>FORM</p>
+
+        <h3 className='text-header'>My Projects</h3>
+        <div className='project-images'>
+            <img className='app-img' src={activity1} alt='activity-tracker app'></img>
+            <img className='app-img' src={meal1} alt='mealventory app'></img>
+        </div>        
     </div>    
   );
 }
