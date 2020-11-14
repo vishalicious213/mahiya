314e2e52958396e1013963a606ef3721768493fb
Vish Singh <vishalicious213@gmail.com>
2020-11-13 16:09:13 -0800

    adding text to AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,6 +7,17 @@ function AboutMe() {
         <div id='byline-container'>
             <p id='byline'>Hi there, I’m Mahiya and my mission is to create a sense of community.</p>
         </div>
+
+        <p className='text-content'>I have worked within the customer service fields in retail, car sales, and administrative positions, where I constantly needed to understand people. For years, I have practiced creating empathy for the problems we face routinely. This naturally brought me to the field of product design where I now solve these problems using design thinking. </p>
+
+        <h3 className='text-header'>What problems am I currently invested in?</h3>
+        <p className='text-content'>The year 2020 has emboldened a lot of societal and political issues that had already existed. The ones I’m occupied with currently are voting-transparency and anti-racism.</p>
+        <p className='text-content'>I am holding monthly meetings on meetup to discuss how we can create a platform to make anti-racism actionable. I’m also working on something that would make information around voting such as who, what, when, where, how, easier to access.</p>
+        <p className='text-content'>I’m a volunteer team-member for the redesign efforts of the DSA Labor website.</p>
+        <p className='text-content'>I’m also coming up with a tool  that friends and colleagues can use to queue their favorite songs insteading of needing to pause and wait while somebody finds their favorite song on YouTube or spotify (ughh!!)</p>
+
+        <h3 className='text-header'>I would love to meet you to hear about your adventures!</h3>
+        <p>FORM</p>
     </div>    
   );
 }
