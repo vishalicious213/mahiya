9a714cec39f4cd12d84231500bb6f8b4d5098a06
Vish Singh <vishalicious213@gmail.com>
2020-11-17 12:44:48 -0800

    updated PlantWateringApp

--- src/components/case-studies/PlantWateringApp.js
+++ src/components/case-studies/PlantWateringApp.js
@@ -21,8 +21,35 @@ function Plants() {
         <p>Building a schedule around ensuring that all your plants are watered is actually pretty difficult. Water my Plants helps to solve those problems by providing an easy to use interface for creating a plant watering schedule that will remind users to water their plants on a regular basis.</p>
 
         <p>Solution overview</p>
+        {/* <div>
+          <img></img>
+          <img></img>
+        </div> */}
 
-        
+        <p>How might we help planters remember to water their plants?</p>
+        <p>The problem is pretty broad. I thought maybe everybody needed watering reminder. But my mother who is an avid gardner seemed to not need any help remembering to water. So I asked:</p>
+        <ol>
+          <li>who needs reminder to water their plants?</li>
+          <li>what are they using now to remember to water?</li>
+          <li>what factors are contributing to their plants growth or deterioration?</li>
+        </ol>
+
+        <p>Key takeaways</p>
+        <p>I spoke with four planters and my mother about their gardening needs, goals, and blockers. Here are my key takeaways:</p>
+        {/* <img></img> */}
+        <ol>
+          <li>Gardeners and those with large backyards tend not to need help with remembering to water.</li>
+          <li>Most outdoor plants are easily taken care of with rain and missing water a few days does not harm them.</li>
+          <li>Planters however, who do not have a large backyard or live indoor in small spaces have drastically different struggles-</li>
+        </ol>
+
+        {/* <img>arrow</img> */}
+
+        <ul>
+          <li>It’s hard to keep up with which plants have been watered, which haven’t and their individual watering needs.</li>
+          <li>It’s hard to identify plants when you have a lot of them at home.</li>
+          <li>It’s challenging to remember the varying needs of each plant because house plants tend to have particular care instructions.</li>
+        </ul>        
       </div>
     )
 }
