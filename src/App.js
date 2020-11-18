fb57ef9d667890c17ee50de2e4e15893dfd51304
Vish Singh <vishalicious213@gmail.com>
2020-11-17 21:47:00 -0800

    commented out components in App - Netlify build is being finicky

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 // import React, { useContext } from 'react'
 import React, { useState } from 'react'
 import Header from './components/Header'
-import Projects from './components/Projects'
+// import Projects from './components/Projects'
 import Footer from './components/Footer'
 import Sidebar from './components/Sidebar'
-import AboutMe from './components/AboutMe'
+// import AboutMe from './components/AboutMe'
 import Plants from './components/case-studies/PlantWateringApp'
 // import { ContentContext } from './components/ContentContext'
 
 function App() {
   // const [content, setContent] = useState(<Projects />)
   // const [content, setContent] = useState(<AboutMe />)
-  const [content, setContent] = useState(<Plants />)
+  const [content] = useState(<Plants />)
 
   // const [content] = useContext(ContentContext)
   // console.log(content)
