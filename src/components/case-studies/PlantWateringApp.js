51ee158dbcf5c359a55c492ad4db13bf17c175e4
Vish Singh <vishalicious213@gmail.com>
2020-11-17 16:16:01 -0800

    updated PlantWateringApp - adding images

--- src/components/case-studies/PlantWateringApp.js
+++ src/components/case-studies/PlantWateringApp.js
@@ -1,43 +1,40 @@
 import React from 'react'
-// import plant1 from '../img/plant-watering-app1.png'
-// import plant2 from '../img/plant-watering-app2.png'
-// import activity1 from '../img/activity-tracker-app1.png'
-// import activity2 from '../img/activity-tracker-app2.png'
-// import meal1 from '../img/mealventory-app1.png'
-// import meal2 from '../img/mealventory-app2.png'
+import img1 from '../../img/case-study/plants/1.png'
+import img2 from '../../img/case-study/plants/2.png'
 import '../../styles/case-studies.css';
 
 function Plants() {
     return (
-      <div id='case-studies'>
+      <div className='case-studies'>
         <h2 id='app-title'>Plant watering app</h2>
 
+        <p className='case-font'>Designed to organize, track, and record plant watering with an emphasis on the wellbeing of indoor plants.</p>
         <p className='case-font-gray'>DURATION</p>
-        <p>6 weeks</p>
+        <p className='darker-text'>6 weeks</p>
         <p className='case-font-gray'>CROSSFUNCTIONAL TEAM</p>
-        <p>Front-end developers, back-end developers, project lead and UX</p>
+        <p className='darker-text'>Front-end developers, back-end developers, project lead and UX</p>
 
         <h3 className='case-section-font'>Problem</h3>
-        <p>Building a schedule around ensuring that all your plants are watered is actually pretty difficult. Water my Plants helps to solve those problems by providing an easy to use interface for creating a plant watering schedule that will remind users to water their plants on a regular basis.</p>
+        <p className='case-font'>Building a schedule around ensuring that all your plants are watered is actually pretty difficult. Water my Plants helps to solve those problems by providing an easy to use interface for creating a plant watering schedule that will remind users to water their plants on a regular basis.</p>
 
         <p className='case-section-font'>Solution overview</p>
-        {/* <div>
-          <img></img>
-          <img></img>
-        </div> */}
+        <div className='app-screen-container'>
+          <img className='app-screen' src={img1} alt='plant watering app screenshot'></img>
+          <img className='app-screen' src={img2} alt='plant watering app screenshot'></img>
+        </div>
 
         <h3 className='case-section-font'>How might we help planters remember to water their plants?</h3>
-        <p>The problem is pretty broad. I thought maybe everybody needed watering reminder. But my mother who is an avid gardner seemed to not need any help remembering to water. So I asked:</p>
-        <ol>
+        <p className='case-font'>The problem is pretty broad. I thought maybe everybody needed watering reminder. But my mother who is an avid gardner seemed to not need any help remembering to water. So I asked:</p>
+        <ol className='alpha-list'>
           <li>who needs reminder to water their plants?</li>
           <li>what are they using now to remember to water?</li>
           <li>what factors are contributing to their plants growth or deterioration?</li>
         </ol>
 
         <h3 className='case-section-font'>Key takeaways</h3>
-        <p>I spoke with four planters and my mother about their gardening needs, goals, and blockers. Here are my key takeaways:</p>
+        <p className='case-font'>I spoke with four planters and my mother about their gardening needs, goals, and blockers. Here are my key takeaways:</p>
         {/* <img></img> */}
-        <ol>
+        <ol className='alpha-list'>
           <li>Gardeners and those with large backyards tend not to need help with remembering to water.</li>
           <li>Most outdoor plants are easily taken care of with rain and missing water a few days does not harm them.</li>
           <li>Planters however, who do not have a large backyard or live indoor in small spaces have drastically different struggles-</li>
@@ -52,19 +49,19 @@ function Plants() {
         </ul>
 
         <h3 className='case-section-font'>Creating empathy</h3>
-        <p>Because I’m not the one experiencing these challanges, I needed to empathize with the planters who do. I defined the typical planter who needs a solution to these challenges.</p>
+        <p className='case-font'>Because I’m not the one experiencing these challanges, I needed to empathize with the planters who do. I defined the typical planter who needs a solution to these challenges.</p>
 
         <h3 className='case-section-font'>The Feng Shui Enthusiast</h3>
         <div>
-          <p>Loves adopting plants, but can’t get them organized to save her life. Her partner thinks she owns too many plants for their small apartment. She feels frustrated because often these plants have different needs and not many ways to tell them apart.</p>
+          <p className='case-font'>Loves adopting plants, but can’t get them organized to save her life. Her partner thinks she owns too many plants for their small apartment. She feels frustrated because often these plants have different needs and not many ways to tell them apart.</p>
           {/* <img></img> */}
         </div>
-        <p>I put myself in her shoes and stopped at every point in her journey to understand her needs, goals, and frustrations.</p>
+        <p className='case-font'>I put myself in her shoes and stopped at every point in her journey to understand her needs, goals, and frustrations.</p>
 
         {/* <img>chart</img> */}
 
         <h3 className='case-section-font'>Information Architecture</h3>
-        <p>Using my new found growth, I was ready to create the userflow using some predetermined tasks.</p>
+        <p className='case-font'>Using my new found growth, I was ready to create the userflow using some predetermined tasks.</p>
         {/* <div>
           <img></img>
           <img></img>
@@ -75,7 +72,7 @@ function Plants() {
         {/* <img>tree diagram</img> */}
 
         <h3 className='case-section-font'>How might we differentiate between watered and unwatered plants?</h3>
-        <p>I had a couples ideas on how to denote a plant that needs water from a plant that’s not ready to be watered yet. I looked at how apps apply different states to selected and deselected items. I also considered symbols as a way to differentiate.</p>
+        <p className='case-font'>I had a couples ideas on how to denote a plant that needs water from a plant that’s not ready to be watered yet. I looked at how apps apply different states to selected and deselected items. I also considered symbols as a way to differentiate.</p>
 
         {/* <div>
           <img></img>
@@ -88,16 +85,16 @@ function Plants() {
         </div> */}
 
         <h3 className='case-section-font'>Tracking watering</h3>
-        <p>Because indoor plant care is individual to each plants, users need to be able to see water history and set watering frequencies for them.</p>
+        <p className='case-font'>Because indoor plant care is individual to each plants, users need to be able to see water history and set watering frequencies for them.</p>
 
         {/* <img></img> */}
 
-        <p>In an earlier exploration, I used filters denoting dehydrated plants from the rest. In this version, tapping on the plant would make the filter go away and it would hence be marked watered.</p>
+        <p className='case-font'>In an earlier exploration, I used filters denoting dehydrated plants from the rest. In this version, tapping on the plant would make the filter go away and it would hence be marked watered.</p>
 
         {/* <img></img> */}
 
         <h3 className='case-section-font'>Final solution</h3>
-        <p>I thought if a water droplet could signify dehydration, then perhaps you could tap that plant and mark it watered. But this is where I needed to get grounded with the goal of my users: they need to see all the plants that are dehydrated at once.</p>
+        <p className='case-font'>I thought if a water droplet could signify dehydration, then perhaps you could tap that plant and mark it watered. But this is where I needed to get grounded with the goal of my users: they need to see all the plants that are dehydrated at once.</p>
 
         {/* <div>
           <img></img>
