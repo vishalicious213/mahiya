28e1e9940fe82e99bed7e0d8194a3b586ff243ae
Vish Singh <vishalicious213@gmail.com>
2020-11-23 13:38:55 -0800

    updated Thoughts

--- src/components/Thoughts.js
+++ src/components/Thoughts.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import '../styles/thoughts.css'
 
-// const thoughts = [
-//     {
-//         title: 'App critique- Amazon’s subscribe and save', 
-//         text: 'This is an analysis of what subscribe and save currently does and what I think it could do to accomplish my goals.'
-//     }
-// ]
-
 function Thoughts() {
-    const Thought = ({ title, text }) => {
+    const Thought = ({ image, title, text, link }) => {
         return (
             <section className='thought'>
                 <div className='thought-img'></div>
                 <p className='thought-title'>{title}</p>
                 <p className='thought-text'>{text}</p>
+                <p className='read-more'>Read more</p>
             </section>
         )
     }
