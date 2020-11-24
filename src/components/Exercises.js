add846bb7420ac3799be15c59e3b1feb3354cf2e
Vish Singh <vishalicious213@gmail.com>
2020-11-23 18:59:19 -0800

    updated Exercises image ex3

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import ex1 from '../img/exercises/ex1.jpg'
-import ex2 from '../img/exercises/ex2.png'
-import ex3 from '../img/exercises/ex3.png'
+import ex2 from '../img/exercises/ex2.jpg'
+import ex3 from '../img/exercises/ex3.jpg'
 import '../styles/exercises.css'
 
 function Exercises() {
