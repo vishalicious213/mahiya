1ac7a3e24f110dd291ec07d998dcf0e5fce477b6
Vish Singh <vishalicious213@gmail.com>
2020-11-24 19:25:29 -0800

    updated Leggings

---
+++ src/components/thoughts/Leggings.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Leggings() {
+    return (
+        <div>
+            <h3>My quest with leggings</h3>
+            <p>There was a time in my life when I used to train heavy. My lower body was so built I almost can’t believe they were mine (haha). Gone are those days. Recently however after a long five-year hiatus from that, I decided to begin some of those old routines. But because it’s been such a long time, I’m needing to train my mind to focus. So, I began watching hours of YouTube videos of women training while I’m training myself. This has been helpful but at one point I found myself needing training clothes like those on the YouTube videos. Only one problem: they’re extremely expensive, hard to find, and no way to know what you are looking for.</p>
+    
+            <p>This is a recording of how I would “UX” the perfect leggings. </p>
+        </div>
+    )
+}
+
+export default Leggings
