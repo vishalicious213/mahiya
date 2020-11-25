759d5759dec5debf93019033daebd64dddf84108
Vish Singh <vishalicious213@gmail.com>
2020-11-24 20:17:29 -0800

    updated Leggings

--- src/components/thoughts/Leggings.js
+++ src/components/thoughts/Leggings.js
@@ -3,33 +3,33 @@ import '../../styles/thoughts.css'
 
 function Leggings() {
     return (
-        <div>
-            <h3>My quest with leggings</h3>
-            <p>There was a time in my life when I used to train heavy. My lower body was so built I almost can’t believe they were mine (haha). Gone are those days. Recently however after a long five-year hiatus from that, I decided to begin some of those old routines. But because it’s been such a long time, I’m needing to train my mind to focus. So, I began watching hours of YouTube videos of women training while I’m training myself. This has been helpful but at one point I found myself needing training clothes like those on the YouTube videos. Only one problem: they’re extremely expensive, hard to find, and no way to know what you are looking for.</p>
-            <p>This is a recording of how I would “UX” the perfect leggings.</p>
+        <div id='thoughts'>
+            <h3 className='thought-title-2'>My quest with leggings</h3>
+            <p className='thought-text-2'>There was a time in my life when I used to train heavy. My lower body was so built I almost can’t believe they were mine (haha). Gone are those days. Recently however after a long five-year hiatus from that, I decided to begin some of those old routines. But because it’s been such a long time, I’m needing to train my mind to focus. So, I began watching hours of YouTube videos of women training while I’m training myself. This has been helpful but at one point I found myself needing training clothes like those on the YouTube videos. Only one problem: they’re extremely expensive, hard to find, and no way to know what you are looking for.</p>
+            <p className='thought-text-2'>This is a recording of how I would "UX" the perfect leggings.</p>
 
-            <h3>My goal</h3>
-            <p>I needed to define the result I expected. So what kind of leggings do I really like?</p>
-            <p>There are ones with a scrunch at your glutes, making them rounder, high waisted ones that make you contain your abdomen, squat proof leggings, regular waisted leggings, cross-wrap waist-banded ones, and v-cuts in the front (my favorite feature). These are only the features that I’m familiar with but I’m certain there are a ton more.</p>
-            <p>If you have shopped at Fabletics, forever21, you might look at a pair on their website and decide because they look good on the model, they will also look good on you. But too many reviews of these leggings are actually negative, and you find yourself in a yearly subscription, paying a lot for low quality, not well-designed leggings. I have purchased my share of leggings from these retailers. In my experience, they sometimes lacked quality, usually didn’t have scrunches, v-cuts, or squat-proof qualities. They seem randomly put together, sometimes designed well unintentionally.</p>
-            <p>So I kept looking and found Alphalete, GymShark, Bombshell Sportswear and other sports attire retailers that are more intentional with their leggings design containing those specific features. Only downside is, they were extremely pricey.</p>
-            <p>So the problem with finding leggings are:</p>
-            <ol>
-                <li>Specific features - expensive</li>
-                <li>Cannot pick your own features or mix and match them in a pair</li>
-                <li>Cannot be intentional with the kinds of leggings we want to wear unless we want to spend x amount of money.</li>
-            </ol>
-            <p>How might we purchase leggings with intention for specific features without spending a ton of money?</p>
+            <div>
+                <h3 className='thought-title-2'>My goal</h3>
+                <p className='thought-text-2'>I needed to define the result I expected. So what kind of leggings do I really like?</p>
+                <p className='thought-text-2'>There are ones with a scrunch at your glutes, making them rounder, high waisted ones that make you contain your abdomen, squat proof leggings, regular waisted leggings, cross-wrap waist-banded ones, and v-cuts in the front (my favorite feature). These are only the features that I’m familiar with but I’m certain there are a ton more.</p>
+                <p className='thought-text-2'>If you have shopped at Fabletics, forever21, you might look at a pair on their website and decide because they look good on the model, they will also look good on you. But too many reviews of these leggings are actually negative, and you find yourself in a yearly subscription, paying a lot for low quality, not well-designed leggings. I have purchased my share of leggings from these retailers. In my experience, they sometimes lacked quality, usually didn’t have scrunches, v-cuts, or squat-proof qualities. They seem randomly put together, sometimes designed well unintentionally.</p>
+                <p className='thought-text-2'>So I kept looking and found Alphalete, GymShark, Bombshell Sportswear and other sports attire retailers that are more intentional with their leggings design containing those specific features. Only downside is, they were extremely pricey.</p>
+                <p className='thought-text-2'>So the problem with finding leggings are:</p>
+                <ol>
+                    <li className='thought-text-2'>Specific features - expensive</li>
+                    <li className='thought-text-2'>Cannot pick your own features or mix and match them in a pair</li>
+                    <li className='thought-text-2'>Cannot be intentional with the kinds of leggings we want to wear unless we want to spend x amount of money.</li>
+                </ol>
+                <p className='thought-text-2'>How might we purchase leggings with intention for specific features without spending a ton of money?</p>
+            </div>
 
-            <h3>Exploration</h3>
-            <p>The features people desire in leggings is something I would need to dig out. For example, I like v-cuts in the front that are midrise and I sometimes like the scrunch. But I don’t want all my leggings to look the same so I would like the freedom to mix and match features. Someone else may only like high-rise v-cuts. The features that could exist and the features that people desire need to be defined.</p>
+            <h3 className='thought-title-2'>Exploration</h3>
+            <p className='thought-text-2'>The features people desire in leggings is something I would need to dig out. For example, I like v-cuts in the front that are midrise and I sometimes like the scrunch. But I don’t want all my leggings to look the same so I would like the freedom to mix and match features. Someone else may only like high-rise v-cuts. The features that could exist and the features that people desire need to be defined.</p>
+            <p className='thought-text-2'>Material is another important element. This could determine how hard you hit the gym on leg days. We’re already straining our minds with having to focus on our muscles; we don’t want to strain our body too by being too hot or too cold, having to constantly fix a wedgie or constantly pulling up these leggings because they’re not squat proof.</p>
+            <p className='thought-text-2'>To find out the right type of material and the specific features people desire, we need a few friends with various body types to try on leggings from Alphalete, gymshark, bombshell sports and other fit-attire retailers. They would use these leggings everyday working out, journaling their experience with for example how they fit during activities, how cool or warm they are, and how they might feel and look. This analysis would then tell us what about these leggings are working and what’s not working.</p>
 
-            <p>Material is another important element. This could determine how hard you hit the gym on leg days. We’re already straining our minds with having to focus on our muscles; we don’t want to strain our body too by being too hot or too cold, having to constantly fix a wedgie or constantly pulling up these leggings because they’re not squat proof.</p>
-
-            <p>To find out the right type of material and the specific features people desire, we need a few friends with various body types to try on leggings from Alphalete, gymshark, bombshell sports and other fit-attire retailers. They would use these leggings everyday working out, journaling their experience with for example how they fit during activities, how cool or warm they are, and how they might feel and look. This analysis would then tell us what about these leggings are working and what’s not working.</p>
-
-            <h3>Final thoughts</h3>
-            <p>Using this data, we would then design a solution where you could choose the things you want in a pair, rather than randomly selecting anything that looks good on your computer screen. I could pick the scrunches that also come with a v-cut front or pick a pair of v-cuts that are not high rise. Possibilities are endless. In this way we could buy the kinds of leggings that make us feel like a million bucks.</p>
+            <h3 className='thought-title-2'>Final thoughts</h3>
+            <p className='thought-text-2'>Using this data, we would then design a solution where you could choose the things you want in a pair, rather than randomly selecting anything that looks good on your computer screen. I could pick the scrunches that also come with a v-cut front or pick a pair of v-cuts that are not high rise. Possibilities are endless. In this way we could buy the kinds of leggings that make us feel like a million bucks.</p>
         </div>
     )
 }
