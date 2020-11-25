6767b9773b652dc5016e720371d28d4478e0c79b
Vish Singh <vishalicious213@gmail.com>
2020-11-24 19:32:50 -0800

    updated Leggings

--- src/components/thoughts/Leggings.js
+++ src/components/thoughts/Leggings.js
@@ -5,8 +5,22 @@ function Leggings() {
         <div>
             <h3>My quest with leggings</h3>
             <p>There was a time in my life when I used to train heavy. My lower body was so built I almost can’t believe they were mine (haha). Gone are those days. Recently however after a long five-year hiatus from that, I decided to begin some of those old routines. But because it’s been such a long time, I’m needing to train my mind to focus. So, I began watching hours of YouTube videos of women training while I’m training myself. This has been helpful but at one point I found myself needing training clothes like those on the YouTube videos. Only one problem: they’re extremely expensive, hard to find, and no way to know what you are looking for.</p>
-    
-            <p>This is a recording of how I would “UX” the perfect leggings. </p>
+            <p>This is a recording of how I would “UX” the perfect leggings.</p>
+
+            <h3>My goal</h3>
+            <p>I needed to define the result I expected. So what kind of leggings do I really like?</p>
+            <p>There are ones with a scrunch at your glutes, making them rounder, high waisted ones that make you contain your abdomen, squat proof leggings, regular waisted leggings, cross-wrap waist-banded ones, and v-cuts in the front (my favorite feature). These are only the features that I’m familiar with but I’m certain there are a ton more.</p>
+            <p>If you have shopped at Fabletics, forever21, you might look at a pair on their website and decide because they look good on the model, they will also look good on you. But too many reviews of these leggings are actually negative, and you find yourself in a yearly subscription, paying a lot for low quality, not well-designed leggings. I have purchased my share of leggings from these retailers. In my experience, they sometimes lacked quality, usually didn’t have scrunches, v-cuts, or squat-proof qualities. They seem randomly put together, sometimes designed well unintentionally.</p>
+            <p>So I kept looking and found Alphalete, GymShark, Bombshell Sportswear and other sports attire retailers that are more intentional with their leggings design containing those specific features. Only downside is, they were extremely pricey.</p>
+            <p>So the problem with finding leggings are:</p>
+            <ol>
+                <li>Specific features - expensive</li>
+                <li>Cannot pick your own features or mix and match them in a pair</li>
+                <li>Cannot be intentional with the kinds of leggings we want to wear unless we want to spend x amount of money.</li>
+            </ol>
+            <p>How might we purchase leggings with intention for specific features without spending a ton of money?</p>
+
+            
         </div>
     )
 }
