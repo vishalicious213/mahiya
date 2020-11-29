6b87ed1cda9bc45957c62ebbcbd51587e27abcd1
Vish Singh <vishalicious213@gmail.com>
2020-11-28 16:54:31 -0800

    updated Projectx3

--- src/components/Projectx3.js
+++ src/components/Projectx3.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import plant1 from '../img/plant-watering-app1.jpg'
+import activity1 from '../img/activity-tracker-app1.jpg'
+import meal1 from '../img/mealventory-app1.jpg'
 import '../styles/projects-footer.css'
 
 function Projectx3() {
@@ -7,13 +10,12 @@ function Projectx3() {
         return (
             <section className='project-3x'>
                 <Link to={link}>
-                    <div className='project-3x-img'></div>
+                    <div className='project-3x-img-container'>
+                        <img src={image} alt={title}></img>
+                    </div>
                 </Link>
                 <p className='thought-title'>{title}</p>
                 <p className='thought-text'>{text}</p>
-                <Link to={link}>
-                    <p className='read-more'>Read more</p>
-                </Link>
             </section>
         )
     }
@@ -24,20 +26,23 @@ function Projectx3() {
             <section id='projects-footer'>
                 <Project 
                     title='Plant watering app' 
-                    text='This is an analysis of what subscribe and save currently does and what I think it could do to accomplish my goals.'
+                    text='design'
                     link='/subscribe'
+                    image={plant1}
                 />
 
                 <Project 
                     title='Activity tracker app' 
-                    text='This is an analysis of what subscribe and save currently does and what I think it could do to accomplish my goals.'
+                    text='strategy, design'
                     link='/subscribe'
+                    image={activity1}
                 />
 
                 <Project 
                     title='Mealventory app' 
-                    text='This is an analysis of what subscribe and save currently does and what I think it could do to accomplish my goals.'
+                    text='design'
                     link='/subscribe'
+                    image={meal1}
                 />
             </section>
         </section>
