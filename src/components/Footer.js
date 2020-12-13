db879f34c8b0da47da4465e18c407de36f0a6f03
Vish Singh <vishalicious213@gmail.com>
2020-12-12 16:33:09 -0800

    centered footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,21 @@ import '../styles/footer.css'
 function Footer() {
     return (
       <div id='footer'>
-        <a target="_blank" rel="noopener noreferrer" href='https://dribbble.com/mahiya17'>
-          <div className='footer-icons' id='globe-icon'></div>
-        </a>
+          <a target="_blank" rel="noopener noreferrer" href='https://dribbble.com/mahiya17'>
+            <div className='footer-icons' id='globe-icon'></div>
+          </a>
 
-        <a target="_blank" rel="noopener noreferrer" href='http://www.linkedin.com/in/mahiya17'>
-          <div className='footer-icons' id='linkedin-icon'></div>
-        </a>
+          <a target="_blank" rel="noopener noreferrer" href='http://www.linkedin.com/in/mahiya17'>
+            <div className='footer-icons' id='linkedin-icon'></div>
+          </a>
 
-        <a target="_blank" rel="noopener noreferrer" href='https://mahiya17.medium.com/'>
-          <div className='footer-icons' id='medium-icon'></div>
-        </a>
+          <a target="_blank" rel="noopener noreferrer" href='https://mahiya17.medium.com/'>
+            <div className='footer-icons' id='medium-icon'></div>
+          </a>
 
-        <a href='mailto:mahiya17@gmail.com'>
-          <div className='footer-icons' id='email-icon'></div>
-        </a>
+          <a href='mailto:mahiya17@gmail.com'>
+            <div className='footer-icons' id='email-icon'></div>
+          </a>
       </div>
     )
 }
