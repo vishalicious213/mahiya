ed981c52c274cd8715115ab59f58879377beb857
Vish Singh <vishalicious213@gmail.com>
2020-12-12 22:52:39 -0800

    got sidebar button class toggle working. have to clean it up

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import leftArrow from '../img/left-arrow.png'
 import { Link } from 'react-router-dom'
 import Resume from '../resources/Mahiya-Hoque.pdf'
 import '../styles/sidebar.css';
 
 function Sidebar() {
+    const [itemPath, setItemPath] = useState('/')
+
+    const pathname = window.location.pathname
+    const url = window.location.href
+    const origin = window.location.origin
+
+    console.log('path ', pathname)
+    console.log('url ', url)
+    console.log('origin ', origin)
+
+    const Star = (props) => {
+      console.log(pathname)
+      console.log(props.itemPath)
+      return(
+      <span className={`${props.itemPath===pathname ? 'red' : 'star'}`}>*</span>
+    )}
+
     return (
       <div id='sidebar'>
         <div id='sidebar-top'>
 
-          <Link to='/'>
-            <p className='side-menu-item'>work</p>
+          <Link className='side-menu-item' to='/' onClick={event => setItemPath('/')}>
+            <p className='hyper'>work</p>
+            {/* <span className='star'>*</span> */}
+            <Star itemPath='/' />
           </Link>
+
+          <Link className='side-menu-item' to='/about-me' onClick={event => setItemPath('/about-me')}>
+            <p className='hyper'>about me</p>
+            {/* <span className='star'>*</span> */}
+            <Star itemPath='/about-me' />
+          </Link>
+
+          {/* <div className='side-menu-item link'>
+              <Link to='/'>
+                <p>work <span>*</span></p>
+              </Link>
+          </div> */}
+          
+          {/* <Link className='side-menu-item' to='/'>
+            <p>work <span>*</span></p>
+          </Link> */}
 
           <Link to='/about-me'>
             <p className='side-menu-item'>about me</p>
