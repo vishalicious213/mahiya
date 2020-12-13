6b91c72be767fcfe92dcc2a20c911f8e8cc6470c
Vish Singh <vishalicious213@gmail.com>
2020-12-13 12:36:47 -0800

    added cancel.svg and made burger menu toggle from open to close

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import burgerIcon from '../img/burger.svg'
+import cancelIcon from '../img/cancel.svg'
 import '../styles/burger.css';
 
 function Burger() {
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const MenuButton = (props) => {
+        if (menuOpen === true) {
+            return (
+                <div>
+                    <img
+                        id='burger-icon' 
+                        src={cancelIcon} 
+                        alt='burger menu'
+                        onClick={event => setMenuOpen(!menuOpen)}
+                    />
+                </div>
+                // <section>
+                //     <div>work</div>
+                //     <div>about me</div>
+                //     <div>my thoughts</div>
+                //     <div>design exercises</div>
+                //     <div>resume</div>
+                // </section>
+            )
+        } else
+            return (
+                <div>
+                    <img
+                        id='burger-icon' 
+                        src={burgerIcon} 
+                        alt='burger menu'
+                        onClick={event => setMenuOpen(!menuOpen)}
+                    />
+                </div>
+            )
+    }
+
+
     return (
         <div id='burger'>
-            <img id='burger-icon' src={burgerIcon} alt='burger menu'></img>
-            {/* <div>work</div>
-            <div>about me</div>
-            <div>my thoughts</div>
-            <div>design exercises</div>
-            <div>resume</div> */}
+            <MenuButton />
+
         </div>
     )
 }
