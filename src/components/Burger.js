52f016be2f3eef919f2228af9d583c7e892277d9
Vish Singh <vishalicious213@gmail.com>
2020-12-13 09:09:27 -0800

    set burger to not display at desktop width and to display at mobile width with display: none and display: block in burger.css

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -4,7 +4,11 @@ import '../styles/burger.css';
 function Burger() {
     return (
         <div id='burger'>
-
+            <div>work</div>
+            <div>about me</div>
+            <div>my thoughts</div>
+            <div>design exercises</div>
+            <div>resume</div>
         </div>
     )
 }
