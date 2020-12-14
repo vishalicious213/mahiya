7161e37d096cbebe718cc3c56b5640bae62c7874
Vish Singh <vishalicious213@gmail.com>
2020-12-14 14:54:43 -0800

    updated ActivityTrackerApp

--- src/components/case-studies/ActivityTrackerApp.js
+++ src/components/case-studies/ActivityTrackerApp.js
@@ -37,7 +37,7 @@ function Activity() {
           <img className='app-screen' src={img2} alt='activity tracker app screenshot'></img>
         </div>
 
-        <h3 className='case-section-font'>What are the current solutions accomplishing and where are they failing?</h3>
+        <h3 className='subsection-font'>What are the current solutions accomplishing and where are they failing?</h3>
         <p className='case-font'>Plenty of workout apps and programs aim to:</p>
         <ul>
           <li>allow logging and tracking workouts, and personalize routines for users</li>
@@ -48,7 +48,7 @@ function Activity() {
           <li>fails to consider the inactive population, made up of the elderly, and people with health conditions, and busy people without a work-life balance.</li>
         </ul>
 
-        <h3 className='case-section-font'>What contributes to stagnation in activity levels?</h3>
+        <h3 className='subsection-font'>What contributes to stagnation in activity levels?</h3>
         <p className='case-font'>Because none of us on the team are fitness trainers, we needed to find an expert who normally trains the elderly and busy adults. The following are some insights we got from Karin who trains this demographic.</p>
         <ol className='alpha-list'>
           <li>In the long-run the older population strive for mobility.</li>
@@ -57,14 +57,14 @@ function Activity() {
           <li>Overtime because we are habitual beings, we become accustomed to the mundane pattern of life, unable to break free from it.</li>
         </ol>
 
-        <h3 className='case-section-font'>How can we relieve this stagnation?</h3>
+        <h3 className='subsection-font'>How can we relieve this stagnation?</h3>
         <ol className='alpha-list'>
           <li>we could create a gaming feature where you build an entire body over time by working out specific parts of your body (aka muscle isolation).</li>
           <li>we could track progress overtime rather than tracking numerical data which can have the opposite effect.</li>
           <li>we could take a page from habit trackers and task management communities and see how they inspire movement.</li>
         </ol>
 
-        <h3 className='case-section-font'>Movement within task management and habit trackers.</h3>
+        <h3 className='subsection-font'>Movement within task management and habit trackers.</h3>
         <p className='case-font'>Google calendar is popularly used to schedule tasks and for following through with plans. It shows completed tasks on the calendar giving a sense of achievement.</p>
 
         <div className='app-screen-container'>
@@ -78,7 +78,7 @@ function Activity() {
           <img className='app-screen' src={img5} alt='journaling on paper'></img>
         </div>
 
-        <h3 className='case-section-font'>How might we track workouts the same way we track our tasks or habits?</h3>
+        <h3 className='subsection-font'>How might we track workouts the same way we track our tasks or habits?</h3>
         <p className='case-font'>Similar to how one might schedule tasks using google calendar and bullet journaling, you could schedule your physical activities in order to track your progression overtime.</p>
 
         <div className='app-screen-container'>
@@ -86,7 +86,7 @@ function Activity() {
           <img className='app-screen' src={img7} alt='app - activity scheduler'></img>
         </div>
 
-        <h3 className='case-section-font'>How might we create a new habit while tracking progress?</h3>
+        <h3 className='subsection-font'>How might we create a new habit while tracking progress?</h3>
         <p className='case-font'>One of the opportunities for habit trackers is they fail to show numerical progression toward a specific goal. For example, I want to quit smoking. If I set a goal for 21 days, I want to see what percentage of the way I have completed. Then say if I happen to fall off, seeing that I completed almost 90% of the way to my 21 day goal, will remind me to keep going.</p>
 
         <div className='app-screen-container'>
@@ -95,15 +95,16 @@ function Activity() {
         </div>
         <img className='two-thirds-img' src={img10} alt='app - dashboard mockup'></img>
 
-        <h3 className='case-section-font'>Pandemic and compromises.</h3>
+        <h3 className='subsection-font'>Pandemic and compromises.</h3>
         <p className='case-font'>Because we went into COVID and having very limited resources, we made many compromises in regards to our solution. For example we had to draw constraints around what workouts we could offer. Eventhough we had the expertise of Karin who could draw up various workout plans for our demographic, we didn’t have enough time to create a whole new API. So we opted to using a prebuilt API that came with a number of workouts.</p>
+        <br></br>
         <p className='case-font'>We also didn’t have enough time to create a carousel of workouts to visually inspire users. So we resorted to using a dropdown with all the workouts in it.</p>
 
-        <div className='app-screen-container'>
+        <div className='app-screen-container app-img-center'>
           <img className='app-screen' src={img11} alt='app - customize workout'></img>
         </div>
 
-        <h3 className='case-section-font'>Final solution</h3>
+        <h3 className='subsection-font'>Final solution</h3>
 
         <Projectx2 app='activity' />
       </div>
