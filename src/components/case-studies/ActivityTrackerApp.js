d91156e4f60b4bab4a679a8d8b03c9556577c6fa
Vish Singh <vishalicious213@gmail.com>
2020-12-14 15:14:09 -0800

    updated ActivityTrackerApp

--- src/components/case-studies/ActivityTrackerApp.js
+++ src/components/case-studies/ActivityTrackerApp.js
@@ -67,33 +67,33 @@ function Activity() {
         <h3 className='subsection-font'>Movement within task management and habit trackers.</h3>
         <p className='case-font'>Google calendar is popularly used to schedule tasks and for following through with plans. It shows completed tasks on the calendar giving a sense of achievement.</p>
 
-        <div className='app-screen-container'>
+        <div className='app-screen-container app-img-center'>
           <img className='third-img' src={img3} alt='Google Calendar schedule'></img>
         </div>
 
         <p className='case-font'>Being an active member of the bullet journal community, I can attest to our movement inspired through seeing  prior achievements. Journaling tracks activities, allowing you to visualize your progress and consistency.</p>
 
         <div className='app-screen-container'>
-          <img className='app-screen' src={img4} alt='journaling in the app'></img>
-          <img className='app-screen' src={img5} alt='journaling on paper'></img>
+          <img className='app-screen-50' src={img4} alt='journaling in the app'></img>
+          <img className='app-screen-50' src={img5} alt='journaling on paper'></img>
         </div>
 
         <h3 className='subsection-font'>How might we track workouts the same way we track our tasks or habits?</h3>
         <p className='case-font'>Similar to how one might schedule tasks using google calendar and bullet journaling, you could schedule your physical activities in order to track your progression overtime.</p>
 
         <div className='app-screen-container'>
-          <img className='app-screen' src={img6} alt='app - activity scheduler'></img>
-          <img className='app-screen' src={img7} alt='app - activity scheduler'></img>
+          <img className='app-screen-50' src={img6} alt='app - activity scheduler'></img>
+          <img className='app-screen-50' src={img7} alt='app - activity scheduler'></img>
         </div>
 
         <h3 className='subsection-font'>How might we create a new habit while tracking progress?</h3>
         <p className='case-font'>One of the opportunities for habit trackers is they fail to show numerical progression toward a specific goal. For example, I want to quit smoking. If I set a goal for 21 days, I want to see what percentage of the way I have completed. Then say if I happen to fall off, seeing that I completed almost 90% of the way to my 21 day goal, will remind me to keep going.</p>
 
         <div className='app-screen-container'>
-          <img className='third-img' src={img8} alt='app - dashboard mockup'></img>
-          <img className='third-img' src={img9} alt='app - dashboard mockup'></img>
+          <img className='quarter-img' src={img8} alt='app - dashboard mockup'></img>
+          <img className='quarter-img' src={img9} alt='app - dashboard mockup'></img>
+          <img className='half-img' src={img10} alt='app - dashboard mockup'></img>
         </div>
-        <img className='two-thirds-img' src={img10} alt='app - dashboard mockup'></img>
 
         <h3 className='subsection-font'>Pandemic and compromises.</h3>
         <p className='case-font'>Because we went into COVID and having very limited resources, we made many compromises in regards to our solution. For example we had to draw constraints around what workouts we could offer. Eventhough we had the expertise of Karin who could draw up various workout plans for our demographic, we didn’t have enough time to create a whole new API. So we opted to using a prebuilt API that came with a number of workouts.</p>
