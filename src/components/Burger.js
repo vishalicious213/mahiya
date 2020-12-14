96ad2683641314106a701cd30c52b39a1bcf5933
Vish Singh <vishalicious213@gmail.com>
2020-12-13 22:51:35 -0800

    burger menu hilights active page in darkturquoise

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -6,7 +6,9 @@ import Resume from '../resources/Mahiya-Hoque.pdf'
 import '../styles/burger.css';
 
 function Burger() {
+    const pathname = window.location.pathname
     const [menuOpen, setMenuOpen] = useState(false)
+    const [itemPath, setItemPath] = useState(pathname)
 
     const MenuButton = () => {
         if (menuOpen === true) {
@@ -33,14 +35,32 @@ function Burger() {
             )
     }
 
+    const MenuItem = (props) => {
+        return (
+            <p className={`${props.itemPath===itemPath ? 'activeMenu' : 'inactiveMenu'}`}>{props.name}</p>
+        )
+    }
+
     const BurgerMenu = () => {
         if (menuOpen === true) {
             return (
                 <section id='burger-menu'>
-                    <Link className='burger-menu-item' onClick={event => setMenuOpen(!menuOpen)} to='/'>work</Link>
-                    <Link className='burger-menu-item' onClick={event => setMenuOpen(!menuOpen)} to='/about-me'>about me</Link>
-                    <Link className='burger-menu-item' onClick={event => setMenuOpen(!menuOpen)} to='/thoughts'>my thoughts</Link>
-                    <Link className='burger-menu-item' onClick={event => setMenuOpen(!menuOpen)} to='/exercises'>design exercises</Link>
+                    <Link className='burger-menu-item' onClick={event => {setMenuOpen(!menuOpen); setItemPath('/')}} to='/'>
+                        <MenuItem name='work' itemPath='/' />
+                    </Link>
+
+                    <Link className='burger-menu-item' onClick={event => {setMenuOpen(!menuOpen); setItemPath('/about-me')}} to='/about-me'>
+                        <MenuItem name='about me' itemPath='/about-me' />
+                    </Link>
+
+                    <Link className='burger-menu-item' onClick={event => {setMenuOpen(!menuOpen); setItemPath('/thoughts')}} to='/thoughts'>
+                        <MenuItem name='my thoughts' itemPath='/thoughts' />
+                    </Link>
+
+                    <Link className='burger-menu-item' onClick={event => {setMenuOpen(!menuOpen); setItemPath('/exercises')}} to='/exercises'>
+                        <MenuItem name='design exercises' itemPath='/exercises' />
+                    </Link>
+
                     <a className='burger-menu-item' onClick={event => setMenuOpen(!menuOpen)} href={Resume} target="_blank" rel="noopener noreferrer">resume</a>
                 </section>
             )
