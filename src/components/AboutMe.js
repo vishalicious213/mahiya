9a5a8b43d19d055130539ed72cf635e286d8509b
Vish Singh <vishalicious213@gmail.com>
2020-12-14 13:53:00 -0800

    updated AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,17 +11,17 @@ function AboutMe() {
 
     return (
         <div id='about-me'>
-            <div id='byline-container'>
-                <p id='byline'>Hi there, I’m Mahiya and my mission is to create a sense of community.</p>
-            </div>
 
             <div id='about-me-top'>
-                <p className='text-content'>I have worked within the customer service fields in retail, car sales, and administrative positions, where I constantly needed to understand people. For years, I have practiced creating empathy for the problems we face routinely. This naturally brought me to the field of product design where I now solve these problems using design thinking. </p>
                 <div id='about-me-img-container'>
                     <img id='about-me-img' src={aboutme} alt='Mahiya Hoque'></img>
                 </div>
+                <div id='byline-container-about'>
+                    <h1 id='byline-about'>Hi there, I’m Mahiya and my mission is to create a sense of community.</h1>
+                </div>
             </div>
 
+            <p className='text-content'>I have worked within the customer service fields in retail, car sales, and administrative positions, where I constantly needed to understand people. For years, I have practiced creating empathy for the problems we face routinely. This naturally brought me to the field of product design where I now solve these problems using design thinking. </p>
             <h3 className='text-header'>What problems am I currently invested in?</h3>
             <p className='text-content'>The year 2020 has emboldened a lot of societal and political issues that had already existed. The ones I’m occupied with currently are voting-transparency and anti-racism.</p>
             <p className='text-content'>I am holding monthly meetings on meetup to discuss how we can create a platform to make anti-racism actionable. I’m also working on something that would make information around voting such as who, what, when, where, how, easier to access.</p>
